1a1f22beae618692b2e587d4f955b54116eac320
SIVAR-CODE <sivadhanushiya0@gmail.com>
2025-09-17 09:12:01 +0530

    Add files via upload

---
+++ q6.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int last digit;
+    printf("enter num");
+    scanf("%d %d",a,b);
+    printf("before swaping a=%d,b=%d",a,b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("after swaping a=%d,b=%d",a,b);
+    return 0;
+}
