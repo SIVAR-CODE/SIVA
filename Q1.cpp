854c404f7aa60c10a02e809eaf73bb9f129ee676
SIVAR-CODE <sivadhanushiya0@gmail.com>
2025-09-17 09:07:35 +0530

    Add files via upload

---
+++ Q1.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int num1,num2,sum;
+    printf("enter afirst num",num1);
+    scanf("%d",& num1);
+    printf("enter a second num,");
+    scanf("%d",&num2);
+    sum=num1+num2;
+    printf("sum of %d is %d",num1,num2,sum);
+    return 0;
+}
